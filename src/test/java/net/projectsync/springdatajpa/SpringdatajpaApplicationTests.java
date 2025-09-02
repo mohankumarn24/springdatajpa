6d307f4655171150b4c0c059fe6753ecee4a8560
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-09-02 21:34:14 +0530

    initial commit

---
+++ src/test/java/net/projectsync/springdatajpa/SpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.projectsync.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
